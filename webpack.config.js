d68e03a92aeca82c260439d5bea941775d1fda51
mazhiyuan <1045467322@qq.com>
2016-11-16 09:56:47 +0800

    q

--- webpack.config.js
+++ webpack.config.js
@@ -9,30 +9,30 @@ module.exports = {
     plugins: [commonsPlugin],
     //页面入口文件配置
     entry: {
-        page1 : 'app/javascript/manga/',
+        manga: ['./app/javascript/manga/manga.js', './app/javascript/manga/picwall.js']
     },
     //入口文件输出配置
     output: {
-        path: 'js',
+        path: 'app/dist',
         filename: '[name].bundle.js'
     },
     module: {
         //加载器配置
         loaders: [
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.js$/, loader: 'jsx-loader?harmony' },
-            { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.js$/, loader: 'babel-loader'},
+            /*{ test: /\.scss$/, loader: 'style!css!sass?sourceMap'},*/
+            {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
         ]
     },
-   /* //其它解决方案配置
+    //其它解决方案配置
     resolve: {
         root: 'E:/github/flux-example/src', //绝对路径
         extensions: ['', '.js', '.json', '.scss'],
         alias: {
-            AppStore : 'js/stores/AppStores.js',
-            ActionType : 'js/actions/ActionType.js',
-            AppAction : 'js/actions/AppAction.js'
+            AppStore: 'js/stores/AppStores.js',
+            ActionType: 'js/actions/ActionType.js',
+            AppAction: 'js/actions/AppAction.js'
         }
-    }*/
+    }
 };
